b490e16b1ddc9f9e622b4f88c06429b21981fc3f
Anand George <mranandgeorge@gmail.com>
2011-12-23 03:01:10 +0000

    Cloud9

--- hello.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.createServer(function(req, res)    {
-        res.writeHead(200,{'content-type':'text/plain'});
-        res.write('Hello World');
-        res.end();
-}).listen(process.env.PORT);
-console.log('Listening on port ' + process.env.PORT);
-        
